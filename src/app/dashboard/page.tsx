09fb8199e1ce39f2d5ea1a3f0b165b946c4197e3
Rashap Goyal <143923646+rashap224@users.noreply.github.com>
2025-08-11 16:21:58 +0530

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -43,7 +43,7 @@ export default async function DashboardPage() {
   const formattedFiles = userData.uploadedFiles.map((file) => ({
     id: file.id,
     s3Key: file.s3Key,
-    filename: file.displayName ?? "Unkown filename",
+  filename: file.displayName ?? "Unknown filename",
     status: file.status,
     clipsCount: file._count.clips,
     createdAt: file.createdAt,
